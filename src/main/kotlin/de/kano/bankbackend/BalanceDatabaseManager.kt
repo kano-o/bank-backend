e259bce0137933409e3b08f329c323a486ed5597
kano <steen4145@gmail.com>
2021-05-03 23:21:25 +0200

    Added: - no idea

--- src/main/kotlin/de/kano/bankbackend/BalanceDatabaseManager.kt
+++ src/main/kotlin/de/kano/bankbackend/BalanceDatabaseManager.kt
@@ -55,3 +55,28 @@ fun withdrawBalance(accountNumber: Long, withdrawal: Double): Double {
         return statement2.resultSet.getDouble("balance")
     }
 }
+
+fun transferBalance(withdrawAccountNumber: Long, depositAccountNumber: Long, transfer: Double): Double {
+    val dbConnection = DriverManager.getConnection("jdbc:sqlite:database.db")
+    dbConnection.use {
+
+        val statement = dbConnection.prepareStatement(
+            "UPDATE accounts SET balance = balance - ? WHERE account_number = ?;" +
+                    "UPDATE accounts SET balance = balance + ? WHERE account_number = ?"
+        )
+        statement.setDouble(1, transfer)
+        statement.setLong(2, withdrawAccountNumber)
+        statement.setDouble(3, transfer)
+        statement.setLong(4, depositAccountNumber)
+        statement.execute()
+
+        val statement2 = dbConnection.prepareStatement(
+            "SELECT balance FROM accounts WHERE account_number = ?;" +
+                    "SELECT balance FROM accounts WHERE account_number = ?"
+        )
+        statement2.setLong(1, withdrawAccountNumber)
+        statement2.setLong(2, depositAccountNumber)
+        statement2.execute()
+
+
+}
